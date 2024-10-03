e47e303538faf2db27a30c9a6a17f11e0f0f9fae
Moein <moein.akbari2003@gmail.com>
2024-10-03 18:57:42 +0330

    Changed function input from list[tuple] to dict.

--- 2-huffman-coding.py
+++ 2-huffman-coding.py
@@ -1,13 +1,13 @@
 # Internal
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 import heapq
 
 # Local
 from infrastructure.BinaryTree import Node
 
 
-def huffman_coding(alphabet_frequencies: List[Tuple[str, int]]) -> Tuple[str, int]:
-    heap_data = [Node((freq, char)) for char, freq in alphabet_frequencies]
+def huffman_coding(alphabet_frequencies: Dict[str, int]) -> Node[Tuple[int, str]]:
+    heap_data = [Node((freq, char)) for char, freq in alphabet_frequencies.items()]
     heapq.heapify(heap_data)
 
     for _ in range(len(alphabet_frequencies) - 1):
@@ -23,12 +23,17 @@ def huffman_coding(alphabet_frequencies: List[Tuple[str, int]]) -> Tuple[str, in
 
     return heap_data
 
+
+def convert_tree_to_code(tree: Node[Tuple[int, str]]) -> Dict[str, int]:
+    pass
+
+
 if __name__ == '__main__':
-    print(huffman_coding([
-            ('a', 45),
-            ('b', 13),
-            ('c', 12),
-            ('d', 16),
-            ('e', 9),
-            ('f', 5)
-    ]))
+    print(huffman_coding({
+            'a': 45,
+            'b': 13,
+            'c': 12,
+            'd': 16,
+            'e': 9,
+            'f': 5
+    }))
