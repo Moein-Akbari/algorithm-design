daf58aea0e35ec9ad2221a18f414cc536053e149
Moein <moein.akbari2003@gmail.com>
2024-10-10 23:13:15 +0330

    checkpoint, renamed some variables.

--- greedy/3-knapsack.py
+++ greedy/3-knapsack.py
@@ -1,37 +1,42 @@
-# In the name of god
+from typing import List, Union
+
 
 class Item:
-    def __init__(self, value, weight, name):
+    def __init__(self, value: float, weight: float, name: str | None):
         self.value = value
         self.weight = weight
-        self.name = name   
+        self.name = name
 
-    def __str__(self):
+    @property
+    def density(self) -> float:
+        return self.value / self.weight
+
+    def __str__(self) -> str:
         return f"({self.name}, {self.weight})"
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"({self.name}, {self.weight})"
 
-def fractional_knapsack(items, max_weight):
-    
+
+def fractional_knapsack(items: List[Item], max_weight: int) -> List[Item]:
     available_weight = max_weight
-    selected_items = []
+    selected_items: List[Item] = []
     total_value = 0
-    items.sort(key=lambda x: x.value / x.weight)
+    items.sort(key=lambda x: x.density)
 
     while available_weight > 0:
         item = items.pop()
-        # print(item)
+
         selected_weight= min(available_weight, item.weight)
         selected_value = selected_weight / item.weight * item.value
+
         selected_items.append(Item(selected_value, selected_weight, item.name))
         total_value += selected_value
         available_weight -= selected_weight
 
-    return (total_value, selected_items)
+    return selected_items
 
 if __name__ == '__main__':
-    tst1 = [Item(50, 50, 0), Item(10, 20, 1), Item(10, 20, 2)]
-    total_value, items = fractional_knapsack(tst1, 50)
-    print(f"total_value = {total_value}, Items: {items}")
+    items = fractional_knapsack([Item(50, 50, 0), Item(10, 20, 1), Item(10, 20, 2)], 50)
+    print(f"total_value = {sum(item.value for item in items)}, items: {items}")
     
