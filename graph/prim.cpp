06795ee157e24ebf495d56cb2e396591dd6aed8d
Moein <moein.akbari2003@gmail.com>
2024-11-08 09:57:30 +0330

    TODO: Not working

--- graph/prim.cpp
+++ graph/prim.cpp
@@ -5,19 +5,21 @@ using namespace std;
 #include <vector>
 #include <iostream>
 #include <set>
+const int INF = 1e9;
 
 class Node {
 private:
     int id;
-    bool isSelected;
+    bool _isSelected;
 public:
     vector<pair<int, Node*>> neighbours;
     Node(int id){
         this -> id = id;
-        this -> isSelected = false;
+        this -> _isSelected = false;
     }
     void addNeighbour(Node* neighbour, int weight);
-    void select() {isSelected = true;}
+    void select() {_isSelected = true;}
+    bool isSelected() {return _isSelected;}
     int getId() {return id;}
 };
 
@@ -47,10 +49,15 @@ void Graph::addEdge(int vertex1, int vertex2, int weight) {
 }
 
 int Graph::minimumSpanningTree() {
-    set<pair<int, int>> bestDistances; //<weight, vertex>
-    int distances[numberOfVertices];
-    int vertex = 0;
+    int vertex = 1;
     int mstWeight = 0;
+    set<pair<int, int>> bestDistances; //<weight, vertex>
+
+    int distances[numberOfVertices];
+    for (int i = 0; i < numberOfVertices; ++i)
+        distances[i] = INF;
+    distances[vertex] = 0;
+
     vertices[vertex] -> select();
     int numberOfSelectedVertices = 1;
     while (numberOfSelectedVertices < this -> numberOfVertices) {
@@ -60,18 +67,27 @@ int Graph::minimumSpanningTree() {
             Node* neighbour = edge.second;
             cout << "neighbour: " << neighbour -> getId() << endl;
             if (distances[neighbour -> getId()] > distances[vertex] + weight) {
+                distances[neighbour -> getId()] = distances[vertex] + weight;
                 bestDistances.erase(make_pair(distances[neighbour -> getId()], neighbour -> getId()));
                 bestDistances.insert(make_pair(distances[vertex] + weight, neighbour -> getId()));
             }
         }
         cout << "out of for!" << endl;
+        // Selecting next minimum vertex
         auto bestDistance = bestDistances.begin();
         vertex = (*bestDistance).second;
+
+        // Skipping the vertex while it is selected.
+        while (vertices[vertex] -> isSelected()){
+            vertex = (*bestDistance).second;
+            bestDistances.erase(*bestDistance);
+            bestDistance = bestDistances.begin();
+        }
+
         mstWeight += (*bestDistance).first;
         vertices[vertex] -> select();
         numberOfSelectedVertices++;
-        bestDistances.erase(bestDistance);
-        cout << "end of while" << endl;
+        cout << "end of while " << vertex << endl;
     }
     return mstWeight;
 }
@@ -79,20 +95,20 @@ int Graph::minimumSpanningTree() {
 int main() {
     Graph graph = Graph(9);
     vector<vector<int>> edges = {
-        {0, 1 ,4},
-        {0, 7, 8},
-        {1, 7, 11},
-        {1, 2, 8},
-        {7, 8, 7},
-        {7, 6, 1},
-        {8, 6, 6}, 
-        {8, 2, 2},
-        {6, 5, 2},
-        {5, 2, 4}, 
-        {5, 3, 14},
-        {5, 4, 10},
-        {2, 3, 7},
-        {3, 4, 9},
+            {0, 1 ,4},
+            {0, 7, 8},
+            {1, 7, 11},
+            {1, 2, 8},
+            {7, 8, 7},
+            {7, 6, 1},
+            {8, 6, 6},
+            {8, 2, 2},
+            {6, 5, 2},
+            {5, 2, 4},
+            {5, 3, 14},
+            {5, 4, 10},
+            {2, 3, 7},
+            {3, 4, 9},
     };
     for (auto edge : edges) {
         graph.addEdge(edge[0], edge[1], edge[2]);
