76426b0c628d9a492d039109479a27220bd671bf
Moein <moein.akbari2003@gmail.com>
2025-01-16 07:30:15 +0330

    idk

--- cas/ca1/2.py
+++ cas/ca1/2.py
@@ -1,16 +1,37 @@
-import math
 
-factorials = [math.factorial(i) for i in range(0, 21)]
-def _max(n0, k0):
-    product = 1
-    for i in range(n0):
-        product = product * (k0 + i)
-    return product / factorials[n0-1]
+from pprint import pprint
 
-n, m = int(input()), int(input())
-result = 1
 
-while _max(n, result) < m:
-    result += 1
-
-print(int(result))
+def minimum_impacts(n, m):
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if i == 1:
+                dp[i][j] = j
+            elif j == 1:
+                dp[i][j] = 1
+            else:
+                low, high = 1, j
+                best = j
+                while low <= high:
+                    mid = (low + high) // 2
+                    breaks = dp[i-1][mid-1]
+                    no_break = dp[i][j-mid]
+                    worst_case = 1 + max(breaks, no_break)
+                    
+                    # Minimize the worst case
+                    best = min(best, worst_case)
+                    
+                    if breaks > no_break:
+                        high = mid - 1
+                    else:
+                        low = mid + 1
+                
+                dp[i][j] = best
+    
+    pprint(dp)
+    return dp[n][m]
+n = int(input())
+m = int(input())
+print(minimum_impacts(n, m))
